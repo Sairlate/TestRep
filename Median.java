a1649bc0e33fa996a66c6eb72a4d9eb76b728d46
Sairlate <dlya.aktivacii@mail.ru>
2023-06-03 14:05:46 +0300

    Нормальный бабл сорт без лишних итераций(вроде как)

--- Median.java
+++ Median.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,38 +6,43 @@ public class Median {
     static void bubbleSort(int[] arr) {
         int n = arr.length;
         int temp = 0;
-        for(int i=0; i < n; i++){
-            for(int j=1; j < (n-i); j++){
-                if(arr[j-1] > arr[j]){
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
+        boolean swapped;
+        for (int i = 0; i < n; i++) {
+            swapped = false;
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
                     arr[j] = temp;
+                    swapped = true;
                 }
             }
+            if (swapped != true) {
+                break;
+            }
+            System.out.println(Arrays.toString(arr));
         }
     }
+
     public static void main(String[] args) {
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[random.nextInt(21)];
         System.out.println("Массив чисел до сортировки:");
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(50); // присваиваем элементу массива рандомное число от 0 до 49
             System.out.print(arr[i] + " ");
         }
         System.out.println();
         bubbleSort(arr);
-        System.out.println("Массив чисвел после сортировки");
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
+        System.out.println("Массив чисел после сортировки");
+        System.out.println(Arrays.toString(arr));
         double num;
-        if(arr.length % 2 == 0){
-            num = (arr[(arr.length/2)-1] + arr[arr.length/2])/2.0;
+        if (arr.length % 2 == 0) {
+            num = (arr[(arr.length / 2) - 1] + arr[arr.length / 2]) / 2.0;
             // можно и так, но вот еще вариант num = (double) (arr[(count/2)-1] + arr[count/2])/2;
-        }else{
-            num = arr[(arr.length-1)/2];
+        } else {
+            num = arr[(arr.length - 1) / 2];
         }
         System.out.println("Медиана: " + num);
     }
